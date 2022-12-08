67e96a138444fa5d42e9dcbef9440df64001dde4
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-09 01:19:41 +0530

    JS code
    Some Information

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;
+myName = "Sushant Khilare";
+console.log(myName);
+
+var myFrdName = "Vinay Bhole";
+console.log(myFrdName); 
+
+myFrdName = "Mayur Kamble";
+console.log(myFrdName); 
---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Sushant Khilare";
+console.log(myFullName);
+
+myFullName = "Sushant Sunil Khilare";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var firstName = "Sushant";
+var lastName = "khilare";
+var age = "23";
+var address = "Pune";
+var pin_code = "411046";
+var gender = "Male";
+var is_married = "No";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
