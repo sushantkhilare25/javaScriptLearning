5509336379973b780d07511a8f474a88f8a071fb
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-10 21:23:14 +0530

    swaping assign.
    name swapping

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,28 @@
+var girl1 = "Sweety";
+var girl2 = "Cutie";
+console.log("=======Before swap=======");
+console.log(girl1,girl2);
+var temp = girl1;
+girl1 = girl2;
+girl2 = temp;
+console.log(girl1,girl2);
+
+
+
+
+
+
+
+
+console.log("======================================");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("=======Before swap=======");
+console.log(num1,num2,num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("=======After swap=======");
+console.log(num1,num2,num3);
