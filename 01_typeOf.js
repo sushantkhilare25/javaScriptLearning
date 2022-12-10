b533a038b01c3e9a69fb91454d4bbd2aeb8d0d9f
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-10 21:25:05 +0530

    typeof
    learnning tyoeof selector.

---
+++ 01_typeOf.js
@@ -0,0 +1,25 @@
+
+var bankName = "SBI Bank";
+console.log(bankName);
+console.log( typeof bankName);
+console.log("----------------------------");
+
+var marks = "90%";
+console.log(marks);
+console.log( typeof marks);
+console.log("----------------------------");
+
+var totalCount = "Hundred and seven";
+console.log(totalCount);
+console.log( typeof totalCount);
+console.log("----------------------------");
+
+var isWorking = true;
+console.log(isWorking);
+console.log( typeof isWorking);
+console.log("----------------------------");
+
+var totalCount = 107;
+console.log(totalCount);
+console.log( typeof totalCount);
+console.log("----------------------------");
