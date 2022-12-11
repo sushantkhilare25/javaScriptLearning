5b9bc30d509dab83f451b4cd616498056f79c963
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-11 20:51:11 +0530

    normal function
    bank details

---
+++ normalFunction.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNumber,location,pincode){
+console.log("My Bank details as are follows:","Bank Name:" + bankName,"...","ac No:" + accountNumber,"...",
+"Branch: " + location,"...","pincode:" +pincode);
+}
+bankDetails("CITY Bank",3456782345,'Pune',431202);
+bankDetails("Axis Bank",7856782345,"Mumbai",441202);
+bankDetails("HDFC Bank",8956782345,"Pune",631202,"Open");
