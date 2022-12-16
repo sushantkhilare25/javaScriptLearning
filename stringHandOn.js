c7a761c666eb574b9b83d0579db81c4f496ee3ad
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-16 16:56:39 +0530

    String basic Assignment
    String with return

---
+++ stringHandOn.js
@@ -0,0 +1,6 @@
+var greet = "     Hey you are doing good, keep it up     "
+console.log("1 ) The given string is:" , greet);
+console.log("2 ) The lenght of given string is: ", greet.length);
+var resultTrim = greet.trim();
+console.log("3 ) After removing the extra spaces in string : ", resultTrim.lenght);
+
