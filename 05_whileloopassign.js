9ee6fb5982f600885821d6da72b1bc8c7844e518
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-22 11:52:24 +0530

    whileloop Assignment
    using whileloop

---
+++ 05_whileloopassign.js
@@ -0,0 +1,67 @@
+console.log(`Using while loop `);
+
+var i=5;
+while (i<=15) {
+    console.log(`${i}`);
+    i++;
+}
+console.log(``);
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+console.log(``);
+var i = 2;
+while (i<=20) {
+    console.log(i);
+    i= i+2;
+}
+console.log(``);
+
+var i = 1;
+while (i<30) {
+    console.log(i);
+    i = i+2;    
+}
+console.log(``);
+
+console.log(``);
+var i=50;
+while (i>=5) {
+    console.log(i);
+    i= i-5;
+    }
+    console.log(``);
+
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i= i+10;
+}
+console.log(``);
+
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i= i-10;
+}
+
+console.log(`Using do while loop`);
+var index = 5;
+do {
+    console.log(index);
+    index = index + 5;
+} while (index<=50);
+console.log(``);
+var index = 10;
+do {
+    console.log(index);
+    index = index + 10;
+} while (index<=100);
+console.log(``);
+var index = 100;
+do {
+    console.log(index);
+    index = index - 10;
+} while (index>=10);
