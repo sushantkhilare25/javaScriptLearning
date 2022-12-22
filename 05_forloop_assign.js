5da96f84c6ab147e67b0f4cc0d42a6720fa4bb5d
sushantkhilare25 <sskhilare25@gmail.com>
2022-12-22 11:49:26 +0530

    for loop Assignment
    using for loop

---
+++ 05_forloop_assign.js
@@ -0,0 +1,25 @@
+for (let index = 5; index<=15; index++) {
+    console.log(index);
+    
+}
+console.log(`---------------------------------------------------`);
+for (let index = 50; index>=40; index--) {
+    console.log(index);
+    
+}
+console.log(`---------------------------------------------------`);
+for (let index = 5; index<=50; index=index+5) {
+    console.log(index);
+    
+}
+
+
+console.log(`---------------------------------------------------`);
+for (let index = 10; index<=100; index=index+10) {
+    console.log(index);
+    
+}
+for (let index = 101; index>=10; index--) {
+    console.log(index);
+    
+}
